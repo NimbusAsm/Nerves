4e8805255a34d922900f2ad355d4fca1915f340b
Dynesshely <catrol@qq.com>
2023-01-25 05:13:54 +0800

    💾 📝 Feat, Chore: 关于 Swagger 的相关配置

---
+++ Nerves.API.Server/ApiVersions.cs
@@ -0,0 +1,12 @@
+﻿namespace Nerves.API.Server;
+
+/// <summary>
+/// Api 版本枚举
+/// </summary>
+public enum ApiVersions
+{
+    /// <summary>
+    /// Version 1
+    /// </summary>
+    V1 = 1
+}
--- Nerves.API.Server/Program.cs
+++ Nerves.API.Server/Program.cs
@@ -1,11 +1,34 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.OpenApi.Models;
+using Nerves.API.Server;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+#if !Debug
+    options =>
+    {
+        var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename), true);
+        //生成多个文档显示
+        typeof(ApiVersions).GetEnumNames().ToList().ForEach(version =>
+        {
+            //添加文档介绍
+            options.SwaggerDoc(version, new OpenApiInfo
+            {
+                Title = $"Nerves",
+                Version = version,
+                Description = $"Version: {version}"
+            });
+        });
+    }
+#endif
+);
 
 var app = builder.Build();
 
@@ -13,7 +36,22 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(
+        options =>
+        {
+
+            /*
+             options.SwaggerEndpoint($"/swagger/V1/swagger.json",$"版本选择:V1");
+            */
+            //如果只有一个版本也要和上方保持一致
+            typeof(ApiVersions).GetEnumNames().ToList().ForEach(version =>
+            {
+                //切换版本操作
+                //参数一是使用的哪个json文件,参数二就是个名字
+                options.SwaggerEndpoint($"/swagger/{version}/swagger.json", version);
+            });
+        }
+    );
 }
 
 app.UseHttpsRedirection();
