6750099f32b7a8467ad8d2d610cc0c861c8063c8
Dynesshely <catrol@qq.com>
2023-04-04 00:31:08 +0800

    🎇 Style: Clean code.

--- Nerves.ApiServer/Program.cs
+++ Nerves.ApiServer/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.OpenApi.Models;
-using Nerves.API.Server;
-using Nerves.API.Server.Services;
+using Nerves.ApiServer;
+using Nerves.ApiServer.Services;
 using System.Reflection;
 
 var configs = new ConfigManager()
@@ -16,16 +16,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
-#if !Debug
+#if DEBUG
 builder.Services.AddSwaggerGen(
     options =>
     {
         var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
         options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename), true);
-        //生成多个文档显示
+        // 生成多个文档显示
         typeof(ApiVersions).GetEnumNames().ToList().ForEach(version =>
         {
-            //添加文档介绍
+            // 添加文档介绍
             options.SwaggerDoc(version, new OpenApiInfo
             {
                 Title = $"Nerves",
@@ -43,17 +43,19 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
+
     app.UseSwaggerUI(
         options =>
         {
             /*
              options.SwaggerEndpoint($"/swagger/V1/swagger.json",$"版本选择:V1");
             */
-            //如果只有一个版本也要和上方保持一致
+            // 如果只有一个版本也要和上方保持一致
+
             typeof(ApiVersions).GetEnumNames().ToList().ForEach(version =>
             {
-                //切换版本操作
-                //参数一是使用的哪个json文件,参数二就是个名字
+                // 切换版本操作
+                // 参数一是使用的哪个 json 文件, 参数二就是个名字
                 options.SwaggerEndpoint($"/swagger/{version}/swagger.json", version);
             });
         }
